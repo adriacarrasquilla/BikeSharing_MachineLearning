552e3e63a191135298d7538fc0f8b0cafd37f18d
Adrià Carrasquilla <carrasquilla.adria@gmail.com>
2019-10-27 11:05:27 +0100

    Modificació apartat C (gràfics anàlisis)

--- apartatC.py
+++ apartatC.py
@@ -12,6 +12,7 @@ import seaborn as sb
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import datetime
+import matplotlib.ticker as ticker
 
 # Visualitzarem només 3 decimals per mostra
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
@@ -69,14 +70,29 @@ for index in range(dataset['dia'].shape[0]):
 		i+=1
 	dataset['dia'][index]=i
 """
+"""
+correlacio = dataset.corr().round(2)
 
+plt.figure(figsize=(10,8))
 
+ax = sb.heatmap(correlacio, annot=True, linewidths=.5, )
+b, t = plt.ylim() # discover the values for bottom and top
+b += 0.5 # Add 0.5 to the bottom
+t -= 0.5 # Subtract 0.5 from the top
+plt.ylim(b, t) # update the ylim(bottom, top) values
+plt.savefig('heatmap_tot_bo.png')
+
+#Executar aquest codi quan s'hagi d'executar tot, sino ens donarà error al eliminar una columna ja eliminada
+
+"""
 #Esborrem atributs que no usarem
 del dataset["dteday"]
 del dataset["instant"]
 del dataset["casual"]
 del dataset["registered"]
 del dataset["atemp"]
+
+
 del dataset['holiday']
 del dataset['windspeed']
 
@@ -119,13 +135,71 @@ dataset.info()
 #per entendre la naturalesa del nostre atribut a predir
 datasetHora['cnt'].describe()
 
-ax = datasetHora[['hr','cnt']].groupby(['hr']).sum().reset_index().plot(kind='bar', figsize=(8, 6),
-                                       legend = False, title ="Lloguer total per hores", 
+ax = datasetHora[['temp','cnt']].groupby(['temp']).sum().reset_index().plot(kind='bar', figsize=(8, 6),
+                                       legend = False, title ="Lloguer total en funció la temperatura", 
                                        color='#a80027', fontsize=12, width=1.5)
 ax.set_facecolor('#ffecea')
-ax.set_xlabel("Hora", fontsize=10)
+ax.set_xlabel("temp", fontsize=10)
 ax.set_ylabel("Cnt", fontsize=10)
 plt.show()
+
+"""
+count = datasetHora['workingday'].value_counts()
+cntCount = datasetHora.groupby('workingday').sum()['cnt']
+print(cntCount)
+print(count)
+res = cntCount/count
+work=['no-laborable', 'laborable']
+
+fig = plt.figure()
+plt.bar(work, res, color = ["#ba343a", "#a80027"])
+plt.xticks(work, ('No Laborable', 'Laborable'))
+
+plt.show()
+"""
+
+def rand_jitter(arr):
+    stdev = .01*(max(arr)-min(arr))
+    return arr + np.random.randn(len(arr)) * stdev
+
+def jitter(x, y, s=20, c='b', marker='o', cmap=None, norm=None, vmin=None, vmax=None, alpha=None, linewidths=None, verts=None, **kwargs):
+    return plt.scatter(rand_jitter(x), rand_jitter(y), s=s, c=c, marker=marker, cmap=cmap, norm=norm, vmin=vmin, vmax=vmax, alpha=alpha, linewidths=linewidths, verts=verts, **kwargs)
+
+
+plt.figure(figsize=(12,6))
+ax = jitter(datasetHora['hr'], datasetHora['cnt'], c = datasetHora['temp'], cmap='inferno')
+plt.xlabel("Hora")
+plt.ylabel("Cnt")
+cbar = plt.colorbar()
+cbar.set_label('temp', rotation=270)
+plt.xticks(datasetHora['hr'])
+plt.savefig("tmp_hour_cnt_jigger")
+
+plt.figure(figsize=(12,6))
+ax = plt.scatter(datasetHora['hr'], datasetHora['cnt'], c = datasetHora['temp'], cmap='inferno')
+plt.xlabel("Hora")
+plt.ylabel("Cnt")
+cbar = plt.colorbar()
+cbar.set_label('temp', rotation=270)
+plt.xticks(datasetHora['hr'])
+plt.savefig("tmp_hour_cnt")
+
+
+"""
+count = datasetHora['weathersit'].value_counts()
+temps=['Clear','Cloudy','Low rainfall', 'High rainfall']
+fig = plt.figure()
+plt.bar(temps, count, color = ["#461220","#ca343a", "#ba343a","#a80027",])
+plt.title("Count del weathersit")
+plt.show()
+"""
+"""
+plt.figure()
+ax = sb.countplot(x='workingday', data=datasetHora)
+ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
+ax.xaxis.set_major_locator(ticker.MultipleLocator(base=5))
+plt.show()
+"""
 
 print (dataset.keys())
 
@@ -158,12 +232,16 @@ print("Mean squeared error: ", MSE)
 print("R2 score: ", r2)
 
 
-
+"""
 plt.figure()
 #ax.set_facecolor('#ffecea')
 ax = plt.scatter(x_t[:,0], y, c='#ba343a', alpha=0.7, edgecolors = 'none', facecolor='#ffecea')
-plt.plot(x_t[:,0], predicted, '#461220')
+#plt.plot(x_t[:,0], predicted, '#461220')
+plt.title("Cnt en funció la temperatura (Normalitzat)")
+plt.xlabel("Temperatura")
+plt.ylabel("Cnt")
 plt.show()
+"""
 
 
 
